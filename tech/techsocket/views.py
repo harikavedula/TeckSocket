ad8d9eff633b9d61cb6af5cd1a2395782092bcdb
Aman-Syed <ammuaman256@gmail.com>
2022-07-25 20:48:09 +0530

    TechSocket

--- tech/techsocket/views.py
+++ tech/techsocket/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def mentions(request):
+    return render(request, 'mentions.html')
+
+
+def teamoverview(request):
+    return render(request, 'teamoverview.html')
